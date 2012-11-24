7d2a174fe02b3ed5ca7a4c290aba19abc29a2d4d
Tom Nealon <tgnealon@gmail.com>
2012-11-24 02:01:55 +0000

    Initial.

---
+++ dmtest/src/loader.java
@@ -0,0 +1,14 @@
+
+public class loader {
+	
+    public static void main(String[] args) {
+        loader myLoader = new loader();
+        myLoader.load();
+     }
+    
+    public static void load() {
+    	
+    }
+    
+    
+}
