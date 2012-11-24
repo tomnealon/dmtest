e825f674193896e397e2f76a9441952d01810fda
Tom Nealon <tgnealon@gmail.com>
2012-11-24 18:50:48 +0000

    Made Reader use generics.

--- dmtest/src/Loader.java
+++ dmtest/src/Loader.java
@@ -10,6 +10,7 @@ public class Loader {
     public static void load() throws IOException {
     	Reader myReader = new Reader("Example.csv");
     	System.out.println(myReader.getCell(0, 3));
+    	System.out.println(myReader.getCell(2, 4));
     	myReader.printRow(0);
     }
     
--- dmtest/src/Reader.java
+++ dmtest/src/Reader.java
@@ -5,36 +5,51 @@ import java.util.ArrayList;
 
 
 public class Reader {
-	private ArrayList<String[]> store = new ArrayList<String[]>();
+	private ArrayList<ArrayList> store = new ArrayList<ArrayList>();
+	private boolean setEmptyToNull = true;
 
 	public Reader(String fileName) throws IOException {
 		BufferedReader CSVFile = new BufferedReader(new FileReader(fileName));
 		String dataRow = CSVFile.readLine();
-		while (dataRow != null){
+		while (dataRow != null) {
 			String[] dataArray = dataRow.split(",");
-			store.add(dataArray);
+			store.add(setTypeList(dataArray));
 		    dataRow = CSVFile.readLine();
 	    }
 		CSVFile.close();
 	}
 	
-	public String[] getRow(int row) {
+	public ArrayList getRow(int row) {
 		return store.get(row);
 	}
 	
-	public String getCell(int row, int col) {
-		return store.get(row)[col];
+	public Object getCell(int row, int col) {
+		return store.get(row).get(col);
 	}
 	
 	public void printRow(int row) {
-		String[] dataArray = getRow(row);
-		for (String item:dataArray) {
+		ArrayList dataArray = getRow(row);
+		for (Object item:dataArray) {
 			System.out.print(item + "\t");
 		}
 	}
 	
 	private boolean detectInt(String cell) {
 		return cell.matches("-?\\d+(\\.\\d+)?");
+	}
+	
+	private ArrayList setTypeList(String[] rowArray) {
+		ArrayList typedRow = new ArrayList();
+		int currentCol = 0;
+		for (String item:rowArray) {
+			if (detectInt(item)) {
+				typedRow.add(Integer.parseInt(item));
+			} else {
+				typedRow.add(item);
+			}
+			currentCol++;
+		}
+		return typedRow;
 	}
 
 }
